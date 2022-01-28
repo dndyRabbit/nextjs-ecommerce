47a8a6653f88a657f675a1b61cd1289621d4a99d
dndyRabbit <rabbit.developer2021@gmail.com>
2022-01-28 22:34:07 +0700

    ad

--- next.config.js
+++ next.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  env: {
-    BASE_URL: "http://localhost:3000",
-    MONGODB_URL:
-      "mongodb+srv://dendyhr:n7farrasi@cluster0.gfxg9.mongodb.net/Rabbit-dev-ecommerce-nextjs?retryWrites=true&w=majority",
-    ACCESS_TOKEN_SECRET: "-h3?nZ+BXa){@%+}k%+S*]-6,Y_Wv=n>9!TZ(SKFL@BsUP=s",
-    REFRESH_TOKEN_SECRET:
-      "F;_8'hZ,<?9a~`.<r4.sY<87J(bZ#:@B?dxZJ(q?C6UP<K8(XaRHkG?]Z[4$V,%PV}n9Zk@x=K}~h9WCj4&$$}*E@XmFTt#A:",
-    PAYPAL_CLIENT_ID:
-      "AWxoLeYxj9LvlhtH2Q_BHzFCVyAI-H8mxZ1qI2hdJbUR9boMn5Lyq6J_erM83V5WtKP78i2mpQybh_t_",
-    CLOUD_UPDATE_PRESET: "next_ecommerce",
-    CLOUD_NAME: "rabbitdev",
-    CLOUD_API: "https://api.cloudinary.com/v1_1/rabbitdev/image/upload",
-  },
-};
+module.exports = {};
